90544a6712baf5b16e5b9d1eb6f92e64a23c3a13
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-23 19:02:56 +0000

    Add the GetCommand.

---
+++ debugger/src/ist/meic/pa/command/GetCommand.java
@@ -0,0 +1,15 @@
+package ist.meic.pa.command;
+
+import ist.meic.pa.MethodCallEntry;
+
+import java.lang.reflect.Field;
+import java.util.Stack;
+
+public class GetCommand implements Command {
+    @Override
+    public Object execute(Stack<MethodCallEntry> stack, Object calledObject, String[] args) throws Exception {
+        Field field = calledObject.getClass().getDeclaredField(args[0]);
+        field.setAccessible(true);
+        return field.get(calledObject);
+    }
+}
