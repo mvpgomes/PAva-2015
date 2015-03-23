ab020384a8e4892ac356df0fc184d94a8720fd19
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-23 11:19:54 +0000

    Update InfoCommand to show object fields.

--- debugger/src/ist/meic/pa/command/InfoCommand.java
+++ debugger/src/ist/meic/pa/command/InfoCommand.java
@@ -1,16 +1,22 @@
 package ist.meic.pa.command;
 
 import ist.meic.pa.MethodCallEntry;
+
+import java.lang.reflect.Field;
 import java.util.Stack;
 
 public class InfoCommand implements Command {
     @Override
     public Object execute(Stack<MethodCallEntry> stack, Object calledObject, String[] args) {
         System.out.print("Called Object:");
-        // Need to print the called object
+        // Print the called object
         System.out.println(calledObject.toString());
-        System.out.println();
-        // Need to print the stack
+        // Print information about the object
+        Field[] fields = calledObject.getClass().getDeclaredFields();
+        for(Field f : fields){
+            System.out.println(f.toString());
+        }
+        // Print the contents of the stack
         System.out.println("Call stack:");
         for(int i=stack.size() - 1; i >= 0; i--) {
             System.out.println(stack.elementAt(i).toString());
