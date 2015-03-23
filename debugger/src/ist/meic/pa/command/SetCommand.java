a6734c73810cd5478624f80b6d788e92eb76d3b6
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-23 19:05:39 +0000

    Add the SetCommand.

---
+++ debugger/src/ist/meic/pa/command/SetCommand.java
@@ -0,0 +1,18 @@
+package ist.meic.pa.command;
+
+import ist.meic.pa.Debugger;
+import ist.meic.pa.MethodCallEntry;
+
+import java.lang.reflect.Field;
+import java.util.Stack;
+
+public class SetCommand implements Command {
+    @Override
+    public Object execute(Stack<MethodCallEntry> stack, Object calledObject, String[] args) throws Exception {
+        Object parsedValue = Debugger.getParameterParser(args[1]);
+        Field field = calledObject.getClass().getDeclaredField(args[0]);
+        field.setAccessible(true);
+        field.set(calledObject,parsedValue);
+        return calledObject;
+    }
+}
