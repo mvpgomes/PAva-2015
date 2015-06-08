d4fac0658e14dce3568fe157dac1e8325bdb5376
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-06-08 16:41:47 +0100

    Add the tests available on the course webpage.

---
+++ commonlisp-apl/tests/inputs/monadic/interval.lisp
@@ -0,0 +1,9 @@
+(locally
+    (declare #+sbcl(sb-ext:muffle-conditions style-warning))
+  (handler-bind
+      (#+sbcl(style-warning #'muffle-warning))
+    (load "load.lisp")
+    ))
+(interval 5)
+(interval 20)
+(interval 500)
